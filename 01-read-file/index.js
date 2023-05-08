ba691c477c366e432e0155c31e6ba547c6e4639f
imperator1440@gmail.com <imperator1440@gmail.com>
2023-05-08 13:09:49 +0300

    fix: redirect read stream to stdout

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,9 +4,7 @@ const path = require('path');
 const filePath = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(filePath, 'utf8');
 
-readStream.on('data', (chunk) => {
-  console.log(chunk);
-});
+readStream.pipe(process.stdout);
 
 readStream.on('end', () => {
   console.log('End Of File');
